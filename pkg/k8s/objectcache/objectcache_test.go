b26c78afe81abefbc2f92415951075db13699199
chankyin <chankyin@bytedance.com>
2023-04-20 11:06:53 +0000

    feat: add `cluster` tag to `object_cache_request` metric

--- pkg/k8s/objectcache/objectcache_test.go
+++ pkg/k8s/objectcache/objectcache_test.go
@@ -86,20 +86,21 @@ func TestGet(t *testing.T) {
 			assert.Equal("bar", fooValue)
 
 			penetrations := metricsOutput.Get("object_cache_request", map[string]string{
-				"error": "Apiserver",
-				"hit":   "false",
+				"cluster": "test-cluster",
+				"error":   "Apiserver",
+				"hit":     "false",
 			})
-			t.Log(metricsOutput.PrintAll())
-			assert.Equal(int64(1), penetrations.Int)
+			assert.Equal(int64(1), penetrations.Int, metricsOutput.PrintAll())
 		} else {
 			assert.True(fooExists)
 			assert.Equal("bar", fooValue)
 
 			hits := metricsOutput.Get("object_cache_request", map[string]string{
-				"error": "nil",
-				"hit":   "true",
+				"cluster": "test-cluster",
+				"error":   "nil",
+				"hit":     "true",
 			})
-			assert.Equal(int64(1), hits.Int)
+			assert.Equal(int64(1), hits.Int, metricsOutput.PrintAll())
 		}
 	}
 }
