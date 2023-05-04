faf73c58805accd87231061d842d5870f7573b39
chankyin <chankyin@bytedance.com>
2023-05-04 10:22:11 +0000

    fix(frontend): prune nesting spans with no actual leaf descendents
    This scenario happens when some objects have all their leaf spans beyond
    the selected time range.

---
+++ pkg/frontend/tf/step/prune_childless.go
@@ -0,0 +1,37 @@
+// Copyright 2023 The Kelemetry Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tfstep
+
+import (
+	"github.com/jaegertracing/jaeger/model"
+
+	tftree "github.com/kubewharf/kelemetry/pkg/frontend/tf/tree"
+	"github.com/kubewharf/kelemetry/pkg/util/zconstants"
+)
+
+type PruneChildlessVisitor struct{}
+
+func (visitor PruneChildlessVisitor) Enter(tree *tftree.SpanTree, span *model.Span) tftree.TreeVisitor {
+	return visitor
+}
+
+// Prune in postorder traversal to recursively remove higher pseudospans without leaves.
+func (visitor PruneChildlessVisitor) Exit(tree *tftree.SpanTree, span *model.Span) {
+	if _, hasTag := model.KeyValues(span.Tags).FindByKey(zconstants.NestLevel); hasTag {
+		if len(tree.Children(span.SpanID)) == 0 && span.SpanID != tree.Root.SpanID {
+			tree.Delete(span.SpanID)
+		}
+	}
+}
