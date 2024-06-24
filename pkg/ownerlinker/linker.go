5851c85eee0d4a666cfb32ffcb9573028138e2cb
xuqingyun <xuqingyun@bytedance.com>
2024-06-24 20:39:08 +0800

    feat(linker): get owner reference from creation snapshot first

--- pkg/ownerlinker/linker.go
+++ pkg/ownerlinker/linker.go
@@ -20,9 +20,12 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/utils/clock"
 
 	"github.com/kubewharf/kelemetry/pkg/aggregator/linker"
+	diffcache "github.com/kubewharf/kelemetry/pkg/diff/cache"
 	"github.com/kubewharf/kelemetry/pkg/k8s"
 	"github.com/kubewharf/kelemetry/pkg/k8s/discovery"
 	"github.com/kubewharf/kelemetry/pkg/k8s/objectcache"
@@ -48,11 +51,21 @@ func (options *options) EnableFlag() *bool { return &options.enable }
 
 type Controller struct {
 	options        options
+	Clock          clock.Clock
 	Logger         logrus.FieldLogger
 	Clients        k8s.Clients
 	DiscoveryCache discovery.DiscoveryCache
 	ObjectCache    *objectcache.ObjectCache
+	DiffCache      diffcache.Cache
+	LinkerMetric   *metrics.Metric[*linkerMetric]
 }
+
+type linkerMetric struct {
+	Cluster string
+	Fetch   string
+}
+
+func (*linkerMetric) MetricName() string { return "owner_linker_lookup" }
 
 var _ manager.Component = &Controller{}
 
@@ -63,6 +76,9 @@ func (ctrl *Controller) Close(ctx context.Context) error { return nil }
 
 func (ctrl *Controller) LinkerName() string { return "owner-linker" }
 func (ctrl *Controller) Lookup(ctx context.Context, object utilobject.Rich) ([]linker.LinkerResult, error) {
+	metric := &linkerMetric{Cluster: object.Cluster, Fetch: "Unknown"}
+	defer ctrl.LinkerMetric.DeferCount(ctrl.Clock.Now(), metric)
+
 	raw := object.Raw
 
 	logger := ctrl.Logger.WithFields(object.AsFields("object"))
@@ -70,16 +86,33 @@ func (ctrl *Controller) Lookup(ctx context.Context, object utilobject.Rich) ([]l
 	if raw == nil {
 		logger.Debug("Fetching dynamic object")
 
-		var err error
-		raw, err = ctrl.ObjectCache.Get(ctx, object.VersionedKey)
+		// try creation snapshot first
+		snapshot, err := ctrl.DiffCache.FetchSnapshot(ctx, object.Key, diffcache.SnapshotNameCreation)
 		if err != nil {
-			return nil, metrics.LabelError(fmt.Errorf("cannot fetch object value from cache: %w", err), "FetchCache")
+			return nil, fmt.Errorf("cannot fallback to snapshot: %w", err)
+		}
+
+		if snapshot != nil {
+			raw = &unstructured.Unstructured{}
+			if err := raw.UnmarshalJSON(snapshot.Value); err != nil {
+				return nil, fmt.Errorf("decode snapshot err: %w", err)
+			}
+			metric.Fetch = "Snapshot"
+		} else {
+			var err error
+			raw, err = ctrl.ObjectCache.Get(ctx, object.VersionedKey)
+			if err != nil {
+				return nil, fmt.Errorf("cannot fetch object value from cache: %w", err)
+			}
+			metric.Fetch = "ObjectCache"
 		}
 
 		if raw == nil {
 			logger.Debug("object no longer exists")
 			return nil, nil
 		}
+	} else {
+		metric.Fetch = "Raw"
 	}
 
 	var results []linker.LinkerResult
